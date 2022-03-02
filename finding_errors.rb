13d497de564bdf9330027c8409b83744c48e32c0
jason <jduong89@gmail.com>
2022-03-02 13:37:35 -0800

    completes exercise 2

--- finding_errors.rb
+++ finding_errors.rb
@@ -1,22 +1,24 @@
 # Exercise 1: Print a message to a pirate.
-put "Hello!"
+puts "Hello!"
 greeting = gets.chomp()
-if answer["Arrr!"]
+if greeting == "Arrr!"
   puts "Go away, pirate."
 else
   puts "Greetings, hater of pirates!"
 end
 # Exercise 2: Write a function to print when each person died in a sentence.
 dickens = ["Charles Dickens," "1870"]
-thackeray = {"William Thackeray", "1863"}
-trollope = {'Anthony Trollope', '1882'}
-hopkins = ["Gerard Manley Hopkins" => "1889"]
+thackeray = ["William Thackeray", "1863"]
+trollope = ["Anthony Trollope", "1882"]
+hopkins = ["Gerard Manley Hopkins", "1889"]
+
 def died(array)
-  name = array[2]
+  name = array[0]
   year = array[1]
-  puts  "#name died in {year}."
-puts died(Dickens)
-puts died(thackeray)
-puts died(trollop)
-puts died(hopkins)
+  puts "#{name} died in #{year}."
 end
+
+puts died(dickens)
+puts died(thackeray)
+puts died(trollope)
+puts died(hopkins)
