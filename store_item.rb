afe098e1d06287ca26a63ea1c471f0965510d4f7
jason <jduong89@gmail.com>
2022-03-02 12:01:10 -0800

    adds new class food

--- store_item.rb
+++ store_item.rb
@@ -9,7 +9,18 @@ class Items
   end
 end
 
+class Food < Items
+  def initialize(input_options)
+    super
+    @shelf_life = input_options[:shelf_life]
+  end
+end
+
 item1 = Items.new(product: "jacket", color: "brown", price: 100)
 puts item1.product
 puts item1.color
 puts item1.price
+
+apple = Food.new(product: "apple", color: "brown", price: 1, shelf_life: "one day")
+
+p apple
